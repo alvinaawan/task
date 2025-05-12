fc9c029119a82326a0ee7bdb68a399a0a218a6ac
alvinaawan <37437200+alvinaawan@users.noreply.github.com>
2025-05-12 09:21:48 +0200

    Update tc1.cy.js

--- cypress/e2e/tc1.cy.js
+++ cypress/e2e/tc1.cy.js
@@ -2,8 +2,12 @@ describe('Counter App', () => {
   it('Verify that the counter is Incremented/Decremented by 1 on pressing the increment or decrement button respectively.', () => {
     cy.visit('http://127.0.0.1:3000/')
     cy.get('#counter').should('have.text', '0');
+
+//Verify the increment and decrement button
     cy.get('#increment-btn').should('exist');
     cy.get('#decrement-btn').should('exist');
+
+//Verify the functionality of the increment button
     cy.get('#increment-btn').click();
     cy.get('#counter').should('have.text', '1');
    
